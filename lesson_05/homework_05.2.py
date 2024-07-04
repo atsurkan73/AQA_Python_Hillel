45b9b2cc789b77962354b97f27e56fde4565f67a
Tsurkan <atsurkan@luxoft.com>
2024-07-04 07:57:36 +0300

    second part of homework_05

---
+++ lesson_05/homework_05.2.py
@@ -0,0 +1,50 @@
+# Given list of tuples (name, surname, age, profession, City location)
+# 1 - Add your new record o the beginning of the given list
+# 2 - In modified list swap elements with indexes 1 and 5 (1<->5). Print result
+# 3 - check that all people in modified list with records indexes 6, 10, 13
+#   have age >=30. Print condition check result
+
+people_records = [
+  ('John', 'Doe', 28, 'Engineer', 'New York'),
+  ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
+  ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
+  ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
+  ('Michael', 'Brown', 22, 'Student', 'Seattle'),
+  ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
+  ('David', 'Miller', 33, 'Software Developer', 'Austin'),
+  ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
+  ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
+  ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
+  ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
+  ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
+  ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
+  ('Ava', 'White', 42, 'Journalist', 'San Diego'),
+  ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
+]
+#1
+people_records.insert (0, ('Harry', 'Nickolson', 39, 'Product Owner', 'Miami'))
+
+print('#1===============================================')
+for item in people_records:
+  print(item)
+
+#2
+people_records[1], people_records[5] = people_records[5], people_records[1]
+
+print('#2===============================================')
+people_records_1 = [print(x) for x in people_records]
+
+#3
+print('#3===============================================')
+index_to_check = [6, 10, 13]
+age = 30
+check = True
+for i in range(len(people_records)):
+  if i == 6 or i == 10 or i == 13:
+    if int(people_records[i][2]) < age:
+      check = False
+      break
+if check:
+  print(f'All verified persons have age more or equal to {age} year')
+else:
+  print(f'Someone from verified persons has age less than {age} year')
