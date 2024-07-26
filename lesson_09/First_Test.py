e431fae1bf6188ed2d702e98d523bacde699219b
Tsurkan <atsurkan@luxoft.com>
2024-07-26 23:18:33 +0300

    added homework_09

--- lesson_09/First_Test.py
+++ lesson_09/First_Test.py
@@ -1,10 +1,7 @@
 import unittest
-
-from utils.assertatin_utils import compare_numbers_with_delta
 import data
 
 class MyTest(unittest.TestCase):
-
 
 # homework_04, task 1 - 3
     def test_search_by_criteria(self):
@@ -79,7 +76,7 @@ class MyTest(unittest.TestCase):
         self.assertTrue(check, f'Someone from verified persons has age less than {age} year')
 
 # homework_06.3
-    def test_new_list_only_string (self):
+    def test_new_list_only_string(self):
 
         list_origin = data.lst1
 
@@ -114,3 +111,103 @@ class MyTest(unittest.TestCase):
         self.assertEqual(sum2, sum3, 'Different sum of even numbers counted with Method2 and Method3')
 
 
+# homework_06.1
+# Порахувати кількість унікальних символів в строці.
+# Якщо їх більше 10 - вивести в консоль True, інакше - False.
+    def test_sum_of_even_numbers_in_list (self):
+
+        string_to_count_uniq_charecters = data.adwentures_of_tom_sawer_formatted.lower()
+
+        task_list = [x for x in string_to_count_uniq_charecters]
+
+        uniq_list = []
+        [uniq_list.append(x) for x in task_list if x not in uniq_list]
+
+        uniq_length = len(uniq_list)
+
+        set_of_charecters = set(string_to_count_uniq_charecters) # second method
+        set_uniq_length = len(set_of_charecters)
+
+        self.assertEqual(uniq_length, set_uniq_length)
+        self.assertEqual(uniq_length, 30)
+
+# homework_07.4
+# Написати функцію, яка приймає рядок та повертає його у зворотному порядку.
+
+    def test_revert_string(self):
+
+        string_to_revert = data.adwentures_of_tom_sawer_formatted
+
+        some_list = list(string_to_revert)
+        length = len(some_list)
+        j = length - 1
+
+        for i in range(int(length / 2)):    # first revert method
+            temp = some_list[i]
+            some_list[i] = some_list[j]
+            some_list[j] = temp
+            j -= 1
+
+        reversed_string_1 = ''.join(map(str, some_list))
+
+        reversed_string_2 = string_to_revert[::-1]  # second revert method
+
+        self.assertEqual(reversed_string_1, reversed_string_2, 'Bad reverse of the string')
+
+
+# homework_07.5
+#Написати функцію, яка приймає список слів та повертає найдовше слово у списку.
+
+    def test_longest_word_in_list (self):
+        some_list = ['France', 'Portugal', 'Spain', 'Germany', 'Netherlands', 'Argentina', 'Philippines', 'China']
+
+        word_max = ''
+        for word in some_list:              # first method
+            if len(word) > len(word_max):
+                word_max = word
+
+        max_len = len(word_max)
+
+        word_max_list = []
+        for word in some_list:
+            if len(word) == max_len:
+                word_max_list.append(word)
+        word_max_length = len(word_max)
+
+        word_max_length_2 = len(max(some_list, key=len))     # second method
+
+        word_max_list_2 = []
+        [word_max_list_2.append(x) for x in some_list if len(x) == word_max_length_2]
+
+        self.assertEqual(word_max_list, word_max_list_2)
+        self.assertEqual(word_max_length, word_max_length_2)
+
+
+# homework_07.10
+# Є ліст з числами, порахуйте сумму усіх ПАРНИХ чисел в цьому лісті
+    def test_count_even_numbers (self):
+
+        lst_origin = [1, 2, 3, 3, 4, 5, 6, 66, 7, 8, 9, 10, 90, 0, 11, 2, 2, 3, 4, -3, -100, 2.99, -4.0]
+
+        lst_of_even = [x for x in lst_origin if x % 2 == 0]
+        lst_of_even_sum = sum(lst_of_even)
+
+        lst_of_even_2 = []
+        lst_of_even_sum_2 = 0
+        for num in lst_origin:
+            if num % 2 == 0:
+                lst_of_even_2.append(num)
+                lst_of_even_sum_2 += num
+
+        self.assertEqual( lst_of_even, lst_of_even_2)
+        self.assertEqual(lst_of_even_sum, lst_of_even_sum_2)
+
+    def test_count_even_numbers_negative (self):
+
+        lst_origin_1 = ['n', 2, 3, 3, 4, 5, 6, 66, 7, 8, 9, 10, 90, 0, 11, 2, 2, 3, 4, -3, -100, 2.99, -4.0]
+
+        with self.assertRaises(TypeError):
+            lst_of_even = [x for x in lst_origin_1 if x % 2 == 0]
+
+if __name__ == '__main__':
+    unittest.main()
